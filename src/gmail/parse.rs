ca23535877beab5963337d9bdbe0b3285f710840
jessun <jessun.pro@gmail.com>
2026-02-03 01:39:13 +0800

    refactor(gmail/parse): simplify xml parsing argument

--- src/gmail/parse.rs
+++ src/gmail/parse.rs
@@ -2,7 +2,7 @@ use anyhow::{Context, Ok, Result};
 use roxmltree::Document;
 
 pub fn get_unread_count(xml_text: &str) -> Result<usize> {
-    let doc = Document::parse(&xml_text).with_context(|| "xml parse error")?;
+    let doc = Document::parse(xml_text).with_context(|| "xml parse error")?;
     let full_count_node = doc
         .descendants()
         .find(|n| n.has_tag_name("fullcount"))
