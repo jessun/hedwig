53e0fec391725d69aaffe58823a4cf0125279533
jessun <jessun.pro@gmail.com>
2026-02-02 18:42:13 +0800

    feat(config): create default config

--- src/config/watcher.rs
+++ src/config/watcher.rs
@@ -1,15 +1,18 @@
 use anyhow::Result;
-use notify::{Config, Event, RecommendedWatcher, RecursiveMode, Watcher};
+use notify::{Config, Error, Event, RecommendedWatcher, RecursiveMode, Watcher};
 use tokio::sync::mpsc;
 
 pub fn run(tx: mpsc::Sender<Event>) -> Result<RecommendedWatcher> {
-    let event_handler = move |res: Result<Event, notify::Error>| match res {
-        Ok(event) => {
-            if let Err(e) = tx.blocking_send(event) {
-                tracing::error!("send error: {}", e);
-            }
+    let event_handler = move |res: Result<Event, notify::Error>| {
+        let Ok(event) = res else {
+            let err = res.unwrap_err();
+            handler_config_not_found(&err);
+            return;
+        };
+
+        if let Err(e) = tx.blocking_send(event) {
+            tracing::warn!("file event send error: {}", e);
         }
-        Err(e) => tracing::error!("watch error: {}", e),
     };
 
     let mut watcher = RecommendedWatcher::new(event_handler, Config::default())?;
@@ -17,8 +20,29 @@ pub fn run(tx: mpsc::Sender<Event>) -> Result<RecommendedWatcher> {
     let path = super::Config::path()?;
 
     if let Err(e) = watcher.watch(&path, RecursiveMode::NonRecursive) {
-        tracing::error!("file watch error: {}", e);
+        handler_config_not_found(&e);
+        if matches!(e.kind, notify::ErrorKind::PathNotFound) {
+            watcher.watch(&path, RecursiveMode::NonRecursive)?;
+        } else {
+            return Err(e.into());
+        }
     };
 
     Ok(watcher)
+}
+
+fn handler_config_not_found(err: &Error) {
+    match err.kind {
+        notify::ErrorKind::PathNotFound => {
+            tracing::warn!("config file not found, generating default template...");
+            if let Err(e) = super::Config::create_default_template() {
+                tracing::error!("failed to create default config: {}", e);
+            } else {
+                tracing::info!("default config generated");
+            }
+        }
+        _ => {
+            tracing::error!("file watch error: {:?}, kind: {:?}", err, err.kind);
+        }
+    }
 }
